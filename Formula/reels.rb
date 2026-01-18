65f4d2dda512c0223b8f1d1a00e95e5717abda59
Nicholas Yeung <nickjyeung@gmail.com>
2026-01-18 04:04:19 -0600

    v1.1.1

--- Formula/reels.rb
+++ Formula/reels.rb
@@ -8,12 +8,12 @@ class Reels < Formula
 
   on_macos do
     depends_on arch: :arm64
-    url "https://github.com/njyeung/reels/releases/download/v1.1.0/reels-darwin-arm64"
+    url "https://github.com/njyeung/reels/releases/download/v1.1.1/reels-darwin-arm64"
     sha256 "04bb90f50bfc9a60a7f0233142ef7fb2ebd29fd10d004a171803536db6571bcb"
   end
 
   on_linux do
-    url "https://github.com/njyeung/reels/releases/download/v1.1.0/reels-linux-amd64"
+    url "https://github.com/njyeung/reels/releases/download/v1.1.1/reels-linux-amd64"
     sha256 "311e2205f708fe5d2aa312be370ffc74fe4b34ddaa24d118b0331309f95a4a8b"
   end
 
