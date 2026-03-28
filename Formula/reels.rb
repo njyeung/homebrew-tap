3aee1de963fae5c2938ba25d9a964a3b42301d79
nick <nickjyeung@gmail.com>
2026-03-28 03:14:46 -0500

    v1.2.5

--- Formula/reels.rb
+++ Formula/reels.rb
@@ -1,20 +1,26 @@
 class Reels < Formula
   desc "Instagram reels in the terminal"
   homepage "https://github.com/njyeung/reels"
-  version "1.2.4"
+  version "1.2.5"
   license "MIT"
 
   depends_on "ffmpeg-full"
 
   on_macos do
     depends_on arch: :arm64
-    url "https://github.com/njyeung/reels/releases/download/v1.2.4/reels-darwin-arm64"
-    sha256 "69f50111be53b083badb5595c06e041ecbd8eca8e7c8f195d974654cb808171c"
+    url "https://github.com/njyeung/reels/releases/download/v1.2.5/reels-darwin-arm64"
+    sha256 "459798d69303da1d0257c3c894930cee951a62404ce1cc75a9153f829d2dc0b4"
   end
 
   on_linux do
-    url "https://github.com/njyeung/reels/releases/download/v1.2.4/reels-linux-amd64"
-    sha256 "1218db5422e5f023a0b2cce7b4bf3b3e416bd075bb01c10eff9abaffc0c26a8b"
+    on_intel do
+      url "https://github.com/njyeung/reels/releases/download/v1.2.5/reels-linux-amd64"
+      sha256 "e564676f3d6bbf62101a51523b254815f1dae4c919df3c0881493cd073818c03"
+    end
+    on_arm do
+      url "https://github.com/njyeung/reels/releases/download/v1.2.5/reels-linux-arm64"
+      sha256 "3fd87dc110ad436a0eb93ad6224a1f17ff7fc99f008190515b60a9e96753a178"
+    end
   end
 
   def install
@@ -28,7 +34,11 @@ class Reels < Formula
         exec "#{bin}/reels-bin" "$@"
       EOS
     else
-      bin.install "reels-linux-amd64" => "reels-bin"
+      if Hardware::CPU.arm?
+        bin.install "reels-linux-arm64" => "reels-bin"
+      else
+        bin.install "reels-linux-amd64" => "reels-bin"
+      end
       # Create wrapper script to set library paths
       (bin/"reels").write <<~EOS
         #!/bin/bash
